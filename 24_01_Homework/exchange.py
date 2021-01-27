f412785e0fe0a245a2cbe96336d5a70bff057787
Sohibjon <sohibjonsa@gmail.com>
2021-01-27 17:00:06 +0500

    Updating

--- 24_01_Homework/exchange.py
+++ 24_01_Homework/exchange.py
@@ -1,14 +1,17 @@
 import requests
+
 from pprint import pprint
 
 def currency_RUB(a,b):
+    l=[]
     for i in range(a, b):
         parameters = f'{i+1}-12-31?symbols=RUB'
         url = f"https://api.exchangeratesapi.io/{parameters}"
         r = requests.get(url)
-
         data1 = r.json()
-        pprint(data1)
-currency_RUB(1998, 2021)
+        l.append(data1)
+    return l
+c = currency_RUB(1998, 2021)
 
+print(c)
 
